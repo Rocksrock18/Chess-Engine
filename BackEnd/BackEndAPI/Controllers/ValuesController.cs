d84e8f6b68b59ff79d9970a19f28455d2c5391c3
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2019-11-23 14:52:46 -0500

    Add files via upload

---
+++ BackEnd/BackEndAPI/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BaseChessEngine;
+
+namespace BackEndAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        Engine engine = new Engine();
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            String move = engine.GetBestMove(5000, true);
+            return move;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+            engine.PerformMove(value);
+            String move = engine.PerformBestMove(5000, true);
+            return move;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

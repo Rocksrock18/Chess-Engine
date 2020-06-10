05c8addf14bc6685977337f954061925960865ba
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2020-06-10 17:44:56 -0400

    Add files via upload

---
+++ BackEnd/Chess 2.0/ChessEngine/Legality.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ChessEngine.Conversion.Pieces;
+
+namespace ChessEngine
+{
+    public class Legality
+    {
+        public static int EnpassantSquare(int beforeSquare, int afterSquare, int piece)
+        {
+            if(piece == (int)WHITE_PAWN || piece == (int)BLACK_PAWN)
+            {
+                if(Math.Abs(beforeSquare/10-afterSquare/10) == 2)
+                {
+                    return (beforeSquare / 10 + afterSquare / 10)*5 + beforeSquare % 10;
+                }
+            }
+            return 0;
+        }
+    }
+}
